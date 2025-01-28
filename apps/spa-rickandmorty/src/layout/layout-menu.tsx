b5dc8dc56b6e1b45ffa5b5ca2202e27f42add2a8
jsur <jsur@gft.com>
2025-01-28 18:20:05 +0100

    update styles

--- apps/spa-rickandmorty/src/layout/layout-menu.tsx
+++ apps/spa-rickandmorty/src/layout/layout-menu.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import { Menu } from '@react-monorepo/ui';
+import { Home, Users } from 'lucide-react';
 
 export default function LayoutMenu() {
   const location = useLocation();
   const menuItems = React.useMemo(
     () => [
       {
+        icon: <Home />,
         label: 'Home',
         to: '/',
       },
       {
+        icon: <Users />,
         label: 'Characters',
         to: '/characters',
       },
@@ -20,15 +23,20 @@ export default function LayoutMenu() {
   );
 
   return (
-    <Menu>
+    <Menu className="bg-white text-black p-4">
       {menuItems.map((item) => (
         <Link
-          className="block"
+          className={`block p-2 rounded-md ${
+            location.pathname === item.to ? 'bg-gray-200' : 'hover:bg-gray-300'
+          }`}
           key={`menu-item-link-${item.label}`}
           to={item.to}
         >
-          <Menu.Item active={location.pathname === item.to}>
-            <span>{item.label}</span>
+          <Menu.Item className="flex items-center space-x-2">
+            <div className="menu-item-content flex items-center space-x-2">
+              <span className="menu-item-icon">{item.icon}</span>
+              <span className="menu-item-label">{item.label}</span>
+            </div>
           </Menu.Item>
         </Link>
       ))}
--- apps/spa-rickandmorty/src/layout/layout.tsx
+++ apps/spa-rickandmorty/src/layout/layout.tsx
@@ -3,16 +3,16 @@ import LayoutMenu from './layout-menu';
 
 export default function Layout() {
   return (
-    <div className="h-screen w-screen flex flex-col overflow-hidden">
-      <div className="flex items-center border-b gap-2">
-        <h1 className="text-3xl p-2">Rick And Morty</h1>
+    <div className="h-screen w-screen flex flex-col overflow-hidden bg-gray-100">
+      <div className="flex items-center border-b gap-4 bg-white shadow-md">
+        <h1 className="text-3xl p-4 text-gray-800">Rick And Morty</h1>
       </div>
       <div className="flex flex-1 overflow-hidden">
-        <div className="w-56 border-r overflow-hidden">
+        <div className="w-56 border-r overflow-hidden bg-white shadow-md">
           <LayoutMenu />
         </div>
-        <div className="flex-1 border-l overflow-hidden">
-          <div className="w-full h-full flex flex-col overflow-hidden">
+        <div className="flex-1 border-l overflow-hidden bg-gray-50">
+          <div className="w-full h-full flex flex-col overflow-hidden p-4">
             <Outlet />
           </div>
         </div>
